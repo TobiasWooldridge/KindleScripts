9d5bf768cd5b02f8bb3c5078d715e65a94070b69
Tobias Wooldridge <tobias@wooldridge.id.au>
2014-10-09 21:54:44 +1030

    Updated generated image script

--- draw/draw-generated-img.py
+++ draw/draw-generated-img.py
@@ -9,23 +9,28 @@ background_color = (255)
 foreground_color = (0)
 
 
-mainFont = ImageFont.truetype("./fonts/LiberationMono-Regular.ttf", 500)
+mainFont = ImageFont.truetype("./fonts/LiberationMono-Regular.ttf", 555)
 subFont = ImageFont.truetype("./fonts/LiberationMono-Regular.ttf", 45)
+subFontTwo = ImageFont.truetype("./fonts/LiberationMono-Regular.ttf", 25)
 
 
-mainText = "{:0>2d}m".format(random.randint(0, 25)).rjust(3)
-subText="Get the app, http://fake.url/"
+mainText = "{: >2d}m".format(random.randint(0, 25)).rjust(3)
+subText="Until the next Loop Bus arrives"
+subTextTwo="Get the app, http://fake.url/"
 
 im = Image.new('L', resolution, background_color)
 
 draw  =  ImageDraw.Draw(im)
-draw.text((25, 65), mainText, font=mainFont, fill=foreground_color)
-draw.text((50, 600), subText, font=subFont, fill=foreground_color)
+draw.text((10, 35), mainText, font=mainFont, fill=foreground_color)
+draw.text((10, 65), "___", font=mainFont, fill=foreground_color)
+draw.text((50, 630), subText, font=subFont, fill=foreground_color)
+draw.text((50, 690), subTextTwo, font=subFontTwo, fill=foreground_color)
 del draw
 
-im = im.transpose(Image.ROTATE_270)
+
+# im = im.transpose(Image.ROTATE_270)
+# image_to_bytes_to_file(im, "/dev/fb0")
 
 
-# im.show()
-image_to_bytes_to_file(im, "/dev/fb0")
+im.show()
 
