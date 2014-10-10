c8d33742f7d9deccec5905312fd0231aa4bcb54c
Tobias Wooldridge <tobias@wooldridge.id.au>
2014-10-10 21:22:14 +1030

    Started adding kindle primitives to python

--- draw/draw-generated-img.py
+++ draw/draw-generated-img.py
@@ -1,31 +1,59 @@
 #!/usr/bin/env python
 from PIL import Image, ImageDraw, ImageFont
 import random
+import platform
+import subprocess
 from image_to_binary import *
 
-resolution = (1024, 768)
+class Kindle:
+    @staticmethod
+    def _command(command):
+        proc = subprocess.Popen(command, stdout=subprocess.PIPE)
+        return proc.stdout.read()
 
-background_color = (255)
-foreground_color = (0)
+    @staticmethod
+    def on_kindle():
+        return 'kindle' in Kindle._command(['uname', '-a'])
 
-mainFont = ImageFont.truetype("./fonts/LiberationMono-Regular.ttf", 555)
-subFont = ImageFont.truetype("./fonts/LiberationMono-Regular.ttf", 45)
-subFontTwo = ImageFont.truetype("./fonts/LiberationMono-Regular.ttf", 25)
+    @staticmethod
+    def battery_capacity():
+        if Kindle.on_kindle():
+            return int(Kindle._command("cat /sys/devices/system/yoshi_battery/yoshi_battery0/battery_capacity")[:-1])
+        else:
+            return random.randint(0, 99);
 
-mainText = "{: >2d}m".format(random.randint(0, 25)).rjust(3)
-subText="Until the next Loop Bus arrives"
-subTextTwo="Get the app, http://fake.url/"
+class FrameGenerator:
+    resolution = (1024, 768)
 
-im = Image.new('L', resolution, background_color)
+    background_color = (255)
+    foreground_color = (0)
 
-draw  =  ImageDraw.Draw(im)
-draw.text((10, 35), mainText, font=mainFont, fill=foreground_color)
-draw.text((10, 65), "___", font=mainFont, fill=foreground_color)
-draw.text((50, 630), subText, font=subFont, fill=foreground_color)
-draw.text((50, 690), subTextTwo, font=subFontTwo, fill=foreground_color)
-del draw
+    mainFont = ImageFont.truetype("./fonts/LiberationMono-Regular.ttf", 555)
+    smallFont = ImageFont.truetype("./fonts/LiberationMono-Regular.ttf", 45)
+    tinyFont = ImageFont.truetype("./fonts/LiberationMono-Regular.ttf", 25)
 
+    def generate_waiting_image(self, time):
+        mainText = "{: >2d}m".format(time).rjust(3)
+        subText="Until the next Loop Bus arrives"
+        subTextTwo="Get the app, http://fake.url/"
 
-im = im.transpose(Image.ROTATE_270)
-image_to_bytes_to_file(im, "/dev/fb0")
+        im = Image.new('L', self.resolution, self.background_color)
+
+        draw  =  ImageDraw.Draw(im)
+        draw.text((10, 35), mainText, font=self.mainFont, fill=self.foreground_color)
+        draw.text((10, 65), "___", font=self.mainFont, fill=self.foreground_color)
+        draw.text((50, 630), subText, font=self.smallFont, fill=self.foreground_color)
+        draw.text((50, 690), subTextTwo, font=self.tinyFont, fill=self.foreground_color)
+        del draw
+
+        return im
+
+fb = FrameGenerator()
+im = fb.generate_waiting_image(Kindle.battery_capacity())
+
+if Kindle.on_kindle():
+    im = im.transpose(Image.ROTATE_270)
+    image_to_bytes_to_file(im, "/dev/fb0")
+else:
+    im.show()
 
